aa31f4e0d942a5b93688fac7a5ca910af687130b
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-12 11:56:25 +0200

    day1/index.php

--- day1/index.php
+++ day1/index.php
@@ -1,10 +1,22 @@
 <?php
 
-function test(bool $a)
+$firstName = 'Samuel';
+
+echo $firstName;
+
+$a = 1;
+$b = 'hello';
+$c = [1, 2, 3, 5];
+
+class Test
 {
 
-    echo 1, 2;
 }
+;
 
+$d = new Test;
+//var_dump() fonction qui affiche les details des objets et des tableaux ( ne retourne aucune valeur)
+var_dump($c);
 
-?>
+//Même chose que var_dump mais avec moins de detail , et peux stocker la valeur dans une variable
+print_r($c);

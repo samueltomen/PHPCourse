84d42b30b8a4497b67705cc8c44c48a2e11cc10c
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-12 12:09:14 +0200

    day1/index.php

--- day1/index.php
+++ day1/index.php
@@ -20,3 +20,49 @@ var_dump($c);
 
 //Même chose que var_dump mais avec moins de detail , et peux stocker la valeur dans une variable
 print_r($c);
+
+//Les types en PHP 
+
+// 1. Les types scalaire
+//Représente une valeur primitives
+// Boolean , Integer , Float , String
+
+// 2. Les types composé
+// Composé de plusieurs types scalaires
+//Array , Object 
+
+// 3. Les Types Speciaux
+//Resource , NULL
+
+//Exemple 
+
+//string 
+$a = 'Hello world';
+
+// integer
+$b = 1;
+
+//float
+$c = 1.25;
+
+//boolean 
+$d = true;
+
+//array
+$e = [1, 2 ,3];
+
+//gettype() fonction qui permet d'affficher le type de valeur
+echo gettype($a);
+echo '<br>';
+echo gettype($b);
+echo '<br>';
+echo gettype($c);
+echo '<br>';
+echo gettype($d);
+echo '<br>';
+echo gettype($e);
+echo '<br>';
+
+//method avec is_ retourne toujours true ou false 
+//exemple avec is_string
+echo is_string($a); //retourne 1 = true 

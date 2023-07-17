3e6a890f64ea4a87d91cd109105d6c45ebef945e
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-17 10:16:28 +0200

    index.php

---
+++ day3/index.php
@@ -0,0 +1,15 @@
+<?php
+//Les instructions conditionnelles if, elseif, else
+
+$conditions = true;
+
+if ($conditions === true) {
+   echo 'dans le if';
+}elseif ($conditions != true){
+    echo "false";
+}else{
+    echo "error";
+}
+
+
+

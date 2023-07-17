ca645bcdd76b3682efde9de34241c7c966729712
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-17 11:44:14 +0200

    Les operateurs pour les nombres

--- day3/index2.php
+++ day3/index2.php
@@ -3,15 +3,42 @@
 $a = 5;
 echo gettype($a);
 echo '</br>';
-echo PHP_INT_MAX;//Valeur maximal qu'on peut stocker avec un entier
+echo PHP_INT_MAX; //Valeur maximal qu'on peut stocker avec un entier
 echo '</br>';
-echo PHP_INT_MIN;//Valeur minimal qu'on peut stocker avec un entier
+echo PHP_INT_MIN; //Valeur minimal qu'on peut stocker avec un entier
 echo '</br>';
-echo PHP_INT_SIZE;//Taille d'un integer en bytes
+echo PHP_INT_SIZE; //Taille d'un integer en bytes
 echo '</br>';
 $b = 0.3;
 echo gettype($b);
+echo '</br>';
 
 //Les opérateurs pour les nombres 
 
+$c = 5;
+$d = 2;
 
+echo $c + $d;
+
+echo '</br>';
+
+echo $c - $d;
+
+echo '</br>';
+
+echo $c / $d;
+
+echo '</br>';
+
+echo $c * $d;
+
+echo '</br>';
+
+echo $c % $d;
+
+echo '</br>';
+echo '</br>';
+echo '</br>';
+echo '</br>';
+echo '</br>';
+echo '</br>';

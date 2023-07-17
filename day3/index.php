398ac312b90edc177230a7dd3c19c081664dd70b
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-17 10:26:15 +0200

    index.php

--- day3/index.php
+++ day3/index.php
@@ -4,12 +4,25 @@
 $conditions = true;
 
 if ($conditions === true) {
-   echo 'dans le if';
-}elseif ($conditions != true){
+    echo 'dans le if';
+} elseif ($conditions != true) {
     echo "false";
-}else{
+} else {
     echo "error";
 }
 
+echo '</br>';
+//Les switch 
 
+$firstName = "Jean"; //Conversion implicite si je met un 1 en string et que je met 1 en integer dans le case 
 
+switch ($firstName) {
+    case 'Jean':
+        echo 'Jean';
+        break;
+    case 'Paul':
+        echo 'Paul';
+        break;
+    default:
+        echo "Default";
+}

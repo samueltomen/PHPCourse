87c96473fdf67f65129e378e6ddab70bc8f26279
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-17 11:34:29 +0200

    introduction au nombres

---
+++ day3/index2.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 5;
+echo gettype($a);
+echo '</br>';
+echo PHP_INT_MAX;//Valeur maximal qu'on peut stocker avec un entier
+echo '</br>';
+echo PHP_INT_MIN;//Valeur minimal qu'on peut stocker avec un entier
+echo '</br>';
+echo PHP_INT_SIZE;//Taille d'un integer en bytes
+echo '</br>';
+$b = 0.3;
+echo gettype($b);
+
+
+

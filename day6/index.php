9bbb8c6193f6dc9c4d1cb399c653c6202dd33941
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-20 10:50:38 +0200

    // Référence et valeur

--- day6/index.php
+++ day6/index.php
@@ -19,7 +19,7 @@ echo "<pre>";
 // Accéder aux valeurs d'un tableau
 echo "<h1>Accéder aux valeurs d'un tableau</h1>";
 
-echo $d[0][1];//2
+echo $d[0][1]; //2
 echo "</br>";
 echo count($d); // Permet de connaitre le nombre d'element dans un tableau ou un objet
 
@@ -34,11 +34,12 @@ echo "<h1>Les opérateurs pour les tableaux</h1>";
 //spread operator
 //Ne marche pas avec des tableau qui ont des chaines de caractères en index
 $a = [1];
-$b = [2,3,4,5,6];
+$b = [2, 3, 4, 5, 6];
 
-print_r([...$a,...$b]); //Eclatage du tableau et repandaison des valeurs
+print_r([...$a, ...$b]); //Eclatage du tableau et repandaison des valeurs
 
-function test($a, $b, ...$rest){
+function test($a, $b, ...$rest)
+{
     return $rest;
 }
 
@@ -47,5 +48,54 @@ print_r(test(...$b));
 // Référence et valeur
 echo "<h1>Référence et valeur</h1>";
 
+// Copie par valeur
+$firstName = "Jean";
+$foo = $firstName;
 
+echo $firstName; //Jean
+echo "</br>";
+echo $foo; //Jean
+$firstName = "Paul";
+echo "</br>";
+echo $firstName; //Paul
+echo "</br>";
+echo $foo; //Jean
+echo "</br>";
+
+echo "----- Copie par reference -----";
+echo "</br>";
+
+// Copie par reference
+$firstName2 = "Jean";
+$foo2 = &$firstName2;
+
+echo $firstName2; //Jean
+echo "</br>";
+echo $foo2; //Jean
+
+$firstName2 = "Paul";
+
+echo "</br>";
+echo $firstName2; //Paul
+echo "</br>";
+echo $foo2; //Paul
+echo "</br>";
+
+$c = [1, 2, 3];
+function func(array $arr): void
+{
+    $arr[0] = 33;
+}
+
+func($c);
+
+print_r($c); //Aucun changement sur la variable $c
+
+function func2(array &$arr): void
+{
+    $arr[0] = 33;
+}
+
+func2($c); //Modification de la valeur de la variable $c car assignation par adresse dans le parametre de la fonction
+print_r($c);
 

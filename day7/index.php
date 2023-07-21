6148e81d7fb244cbc2831b58bc14782ac6d36d7a
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-21 13:06:11 +0200

    Trier des tableaux par valeur

--- day7/index.php
+++ day7/index.php
@@ -41,3 +41,43 @@ print_r($arr);
 print_r($arr2); // Affiche le contenu du tableau $arr2 (la portion extraite).
 
 echo "</pre>";
+
+
+
+// Trier des tableaux par valeur
+// Trier des tableaux par valeur
+echo "<h1>Trier des tableaux par valeur</h1>";
+
+echo "<pre>";
+
+$a = [22, 1, 33, 234, 12, 45];
+$b = ['test', 'Test', 'Aa', 'B', 'b', 'foo', 'Foo'];
+$c = ['a' => 'b', 'foo' => 'bar', 'Hello' => 'world', 'Ciao' => 'Hello'];
+
+// Tri par valeur et non préservation des clés
+sort($a); // Trie $a en ordre croissant, modification du tableau d'origine (non préservation des clés).
+sort($b); // Trie $b en ordre alphabétique (sensible à la casse), modification du tableau d'origine (non préservation des clés).
+sort($c); // Trie $c en ordre alphabétique des valeurs, modification du tableau d'origine (non préservation des clés).
+
+// Tri par valeur et préservation des clés
+asort($a); // Trie $a en ordre croissant, en préservant les clés.
+asort($b); // Trie $b en ordre alphabétique (sensible à la casse), en préservant les clés.
+asort($c); // Trie $c en ordre alphabétique des valeurs, en préservant les clés.
+
+// Tri inverse par valeur et préservation des clés
+arsort($a); // Trie $a en ordre décroissant, en préservant les clés.
+arsort($b); // Trie $b en ordre alphabétique inverse (sensible à la casse), en préservant les clés.
+arsort($c); // Trie $c en ordre alphabétique inverse des valeurs, en préservant les clés.
+
+// Tri personnalisé par valeur et non préservation des clés
+usort($b, 'strcasecmp'); // Trie $b en ordre alphabétique (insensible à la casse) avec fonction de comparaison personnalisée (non préservation des clés).
+usort($b, fn($val1, $val2) => strcmp($val1, $val2)); // Trie $b en ordre alphabétique (sensible à la casse) avec fonction de comparaison personnalisée (non préservation des clés).
+
+// Tri personnalisé par valeur et préservation des clés
+uasort($c, fn($val1, $val2) => strcasecmp($val1, $val2)); // Trie $c en ordre alphabétique (insensible à la casse) avec fonction de comparaison personnalisée, en préservant les clés.
+
+// Affichage des résultats
+print_r($b); // Affiche le contenu de $b après le tri.
+print_r($c); // Affiche le contenu de $c après le tri.
+
+echo "</pre>";

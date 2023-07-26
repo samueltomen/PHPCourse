7ab45dae396fddc8cc2c6878f4232c1053c78604
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-26 12:17:57 +0200

    Mise en place

--- projet1/index.php
+++ projet1/index.php
@@ -6,13 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/style.css">
     <script defer src="public/js/index.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
     <title>Todo</title>
 </head>
 
 <body>
-    <header>HEADER</header>
-    <div class="content">CONTENT</div>
-    <footer>FOOTER</footer>
+    <div class="container">
+        <header>
+            <div class="logo">
+                Dyma Todo
+            </div>
+        </header>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+        <footer>
+            <p>
+                © COPYRIGHT 2023 - Tous Droits Réservés
+            </p>
+        </footer>
+    </div>
 
 </body>
 

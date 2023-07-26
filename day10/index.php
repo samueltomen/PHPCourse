e8e6e32bda0da6f3020721f54d54793543df15c7
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-26 10:54:35 +0200

    Lire et modifier les headers ajoutés par PHP

--- day10/index.php
+++ day10/index.php
@@ -5,3 +5,22 @@ echo "<h1>Lire les requêtes en PHP</h1>";
 
 $a = 1;
 echo $GLOBALS['a']; //Super Global
+
+echo "<pre>";
+// print_r($_SERVER) ;
+
+echo "</pre>";
+
+// Lire et modifier les headers ajoutés par PHP
+echo "<h1>Lire et modifier les headers ajoutés par PHP</h1>";
+
+echo "<pre>";
+
+
+
+
+
+echo "</pre>";
+
+
+

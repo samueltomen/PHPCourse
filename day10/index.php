0d97364d0260cbcc3be323549b1063879013a34b
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-26 11:04:38 +0200

    // Lire et modifier les headers ajoutés par PHP

--- day10/index.php
+++ day10/index.php
@@ -4,10 +4,10 @@
 echo "<h1>Lire les requêtes en PHP</h1>";
 
 $a = 1;
-echo $GLOBALS['a']; //Super Global
+echo $GLOBALS['a']; // Super Global (Accède à une variable globale)
 
 echo "<pre>";
-// print_r($_SERVER) ;
+// print_r($_SERVER) ; // Affiche le contenu de la superglobale $_SERVER
 
 echo "</pre>";
 
@@ -16,11 +16,14 @@ echo "<h1>Lire et modifier les headers ajoutés par PHP</h1>";
 
 echo "<pre>";
 
+// header_remove('X-Powered-By'); // Supprime un header spécifié (ici, 'X-Powered-By')
+// header('Content-type: text/html'); // Définit le header Content-type à 'text/html'
+// header('Content-type: text/plain',false,500); // Définit le header Content-type à 'text/plain' avec le code de statut 500
 
-
-
+print_r(headers_list()); // Affiche la liste des headers envoyés au client
 
 echo "</pre>";
+
 
 
 

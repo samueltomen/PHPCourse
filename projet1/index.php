eb9640bf713f65663b559aa228070e5d688854b4
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-26 11:44:08 +0200

    starting

--- projet1/index.php
+++ projet1/index.php
@@ -1,2 +1,19 @@
-<?php 
-//PROJET TODO
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+    <script defer src="public/js/index.js"></script>
+    <title>Todo</title>
+</head>
+
+<body>
+    <header>HEADER</header>
+    <div class="content">CONTENT</div>
+    <footer>FOOTER</footer>
+
+</body>
+
+</html>

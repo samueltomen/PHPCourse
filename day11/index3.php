0c1120b92758ae06f0c28a4360f728538aea873f
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-27 14:57:51 +0200

    Lecture et écriture en mode binaire

--- day11/index3.php
+++ day11/index3.php
@@ -1,4 +1,6 @@
+<pre>
 <?php
+
 
 // Introduction au système de fichiers
 echo "<h1>Introduction au système de fichiers</h1>";
@@ -20,4 +22,21 @@ echo "<br>";
 // Lire et écrire dans un fichier
 echo "<h1>Lire et écrire dans un fichier</h1>";
 
+readfile($filename);
+// Lecture du contenu du fichier "text.txt" et affichage directement dans la sortie sans stockage en mémoire.
 
+file($filename);
+// Lecture du fichier "text.txt" et stockage de toutes les lignes dans un tableau. Chaque élément du tableau correspond à une ligne du fichier.
+
+file_get_contents($filename);
+// Lecture du fichier "text.txt" et renvoie de son contenu sous forme de chaîne de caractères. Le contenu complet est stocké en mémoire.
+
+file_put_contents($filename, 'Je suis une licorne', FILE_APPEND);
+// Ajoute la chaîne de caractères "Je suis une licorne" à la fin du fichier "text.txt" spécifié par le chemin "$filename". Si le fichier n'existe pas, il sera créé. Le paramètre FILE_APPEND permet de conserver le contenu précédent du fichier et d'ajouter le nouveau contenu à la suite.
+
+// Lecture et écriture en mode binaire
+echo "<h1>Lecture et écriture en mode binaire</h1>";
+
+
+?>
+</pre>

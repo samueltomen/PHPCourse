f9ec0bf85ae0fb10e0f6ff425c6e45d1af205ba2
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-07-31 12:26:08 +0200

    Implémentation de la logique du formulaire

--- projet1/index.php
+++ projet1/index.php
@@ -1,3 +1,25 @@
+<?php
+
+const ERROR_REQUIRED = "Veuillez renseigner une todo";
+const ERROR_TOO_SHORT = "Veuillez entrer au moins 5 caractères";
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $todo = $_POST['todo'] ?? '';
+
+    if (!$todo) {
+        $error = ERROR_REQUIRED;
+    } elseif (mb_strlen($todo) < 5) {
+        $error = ERROR_TOO_SHORT;
+    }
+}
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -12,7 +34,13 @@
         <div class="content">
             <div class="todo-container">
                 <h1>Ma Todo</h1>
-                <div class="todo-form"></div>
+                <form action="/" method="POST" class="todo-form">
+                    <input name="todo" type="text">
+                    <button class="btn btn-primary">Ajouter</button>
+                </form>
+                <?php if ($error) : ?>
+                    <p class="text-danger"><?= $error ?></p>
+                <?php endif; ?>
                 <div class="todo-list"></div>
             </div>
         </div>

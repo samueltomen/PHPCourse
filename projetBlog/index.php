6a036b2f2da66fb1db338c21bb10a53589e12e55
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-08-03 16:34:24 +0200

    Afficher par categories

--- projetBlog/index.php
+++ projetBlog/index.php
@@ -1,8 +1,28 @@
 <?php
 $filename = __DIR__ . '/data/articles.json';
+$articles = [];
+$categories = [];
 
 if (file_exists($filename)) {
     $articles = json_decode(file_get_contents($filename), true ?? []);
+    $cattmp = array_map(fn ($a) => $a['category'], $articles);
+    $categories = array_reduce($cattmp, function ($acc, $cat) {
+        if (isset($acc[$cat])) {
+            $acc[$cat]++;
+        } else {
+            $acc[$cat] = 1;
+        }
+        return $acc;
+    }, []);
+
+    $articlePerCategories = array_reduce($articles, function ($acc, $article) {
+        if (isset($acc[$article['category']])) {
+            $acc[$article['category']] = [...$acc[$article['category']], $article];
+        } else {
+            $acc[$article['category']] = [$article];
+        }
+        return $acc;
+    }, []);
 }
 
 ?>
@@ -21,17 +41,22 @@ if (file_exists($filename)) {
     <div class="container">
         <?php require_once('./includes/header.php') ?>
         <div class="content">
-            <div class="articles-container">
-                <?php foreach ($articles as $a) : ?>
-                    <div class="article block">
-                        <div class="overflow">
-                            <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
-                        </div>
-                        <h2><?= $a['title'] ?></h2>
 
+
+            <div class="category-container">
+                <?php foreach ($categories as $cat => $num) : ?>
+                    <h2 class="p-10"><?= $cat ?></h2>
+                    <div class="articles-container">
+                        <?php foreach ($articlePerCategories[$cat] as $a) : ?>
+                            <div class="article block">
+                                <div class="overflow">
+                                    <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+                                </div>
+                                <h3><?= $a['title'] ?></h3>
+                            </div>
+                        <?php endforeach; ?>
                     </div>
                 <?php endforeach ?>
-
             </div>
         </div>
         <?php require_once('./includes/footer.php') ?>

3f591f36fa868fb1c9967a9c44b08ebda2e0a84a
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-08-03 15:33:20 +0200

    Afficher par catégorie

--- projetBlog/index.php
+++ projetBlog/index.php
@@ -1,3 +1,13 @@
+<?php
+$filename = __DIR__ . '/data/articles.json';
+
+if (file_exists($filename)) {
+    $articles = json_decode(file_get_contents($filename), true ?? []);
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -11,7 +21,18 @@
     <div class="container">
         <?php require_once('./includes/header.php') ?>
         <div class="content">
+            <div class="articles-container">
+                <?php foreach ($articles as $a) : ?>
+                    <div class="article block">
+                        <div class="overflow">
+                            <div class="img-container" style="background-image:url(<?= $a['image'] ?>"></div>
+                        </div>
+                        <h2><?= $a['title'] ?></h2>
 
+                    </div>
+                <?php endforeach ?>
+
+            </div>
         </div>
         <?php require_once('./includes/footer.php') ?>
     </div>

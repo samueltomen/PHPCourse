14a3bcfb44dd043b23ed901bc3fbe565c66faac7
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-08-14 11:48:07 +0200

    requêtes préparées et execution

--- PDO/day1/index.php
+++ PDO/day1/index.php
@@ -14,5 +14,34 @@
     } catch (PDOException $e) {
         echo "error :" . $e->getMessage();
     };
+
+    $user = [
+        'firstname' => 'Jean5',
+        'lastname' => 'Pierre',
+        'email' => 'jeanpierre@gmail.com',
+        'password' => '123'
+    ];
+
+    $statement = $pdo->prepare('INSERT INTO user VALUES (
+    DEFAULT,
+    :firstname,
+    :lastname,
+    :email,
+    :password
+    )');
+    // $statement->bindValue(1, $user['firstname']);
+    // $statement->bindValue(2, $user['lastname']);
+    // $statement->bindValue(3, $user['email']);
+    // $statement->bindValue(4, $user['password']);
+
+
+    $statement->bindParam(':firstname', $user['firstname']);
+    $statement->bindParam(':lastname', $user['lastname']);
+    $statement->bindParam(':email', $user['email']);
+    $statement->bindParam(':password', $user['password']);
+    $statement->execute();
+
+
     ?>
+
 </pre>

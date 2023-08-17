5a0b080ff02cd87f6eb8a75545a16ddcf5a18e3e
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-08-17 12:16:09 +0200

    autoload et composer

--- ComposerEtAutoload/index.php
+++ ComposerEtAutoload/index.php
@@ -1,7 +1,20 @@
-<?php
+<pre>
 
-spl_autoload_register(function ($className) {
-    require __DIR__ . '/' . str_replace('\\', '/', $className) . '.php';
-});
+    <?php
 
-$db = new \database\Database();
+    // spl_autoload_register(function ($className) {
+    //     require __DIR__ . '/' . str_replace('\\', '/', $className) . '.php';
+    // });
+
+    // $db = new \database\Database();
+
+    require './vendor/autoload.php';
+
+    use Symfony\Component\ErrorHandler\Debug;
+
+    Debug::enable();
+
+    $var();
+
+    ?>
+</pre>

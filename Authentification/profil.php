3e6fd797ac25a2345086f301810c5182bd229427
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-08-21 11:19:25 +0200

    Mise en place

---
+++ Authentification/profil.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="login.php">Login</a>
+        <a href="logout.php">Logout</a>
+        <a href="profil.php">Profil</a>
+        <a href="register.php">Register</a>
+    </nav>
+
+    <h1>PROFIL</h1>
+
+</body>
+
+</html>

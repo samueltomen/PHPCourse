dff3d16785aea0b1690dd148fd72cafc0258f56d
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-08-25 15:44:00 +0200

    menu animation

--- projetBlog/public/js/index.js
+++ projetBlog/public/js/index.js
@@ -1,1 +1,6 @@
-console.log('Hello world !');
+const headerMobileButton = document.querySelector('.header-mobile-icon');
+const headerMobileList = document.querySelector('.header-mobile-list');
+
+headerMobileButton.addEventListener('click',()=>{
+    headerMobileList.classList.toggle('show');
+})
